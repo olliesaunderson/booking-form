f7d0332f1f5b6a0febca0c9af41656010fcdbaca
Ollie Saunderson <44266343+olliesaunderson@users.noreply.github.com>
2021-02-04 15:03:30 +0000

    Update db.php

--- Assessment2Site/utils/db.php
+++ Assessment2Site/utils/db.php
@@ -6,7 +6,7 @@
 $host = 'dragon.ukc.ac.uk';
 $dbname = 'octs2';
 $user = 'octs2';
-$password = 'uboth*n';
+$password = '****';
 
 $conn = new PDO("mysql:host=$host;dbname=$dbname", $user, $password);
 $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
